91a1f665c6116ad6244f3311143ebd5993d272e9
Emma <emmabille16@gmail.com>
2023-08-24 14:10:50 +0200

    yy

--- js/product.js
+++ js/product.js
@@ -18,7 +18,7 @@ fetch("https://kea-alt-del.dk/t7/api/products/1550")
 
 function showProduct(product) {
   console.log(product);
-  document.querySelector("img").src = "https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp";
+  document.querySelector(".img_product").src = "https://kea-alt-del.dk/t7/images/webp/640/${product.1550}.webp";
   document.querySelector(".purchaseBox h3").textContent = product.productdisplayname;
   document.querySelector(".purchaseBox .brand").textContent = product.brandname;
   document.querySelector(".purchaseBox .subcat").textContent = product.articletype;
