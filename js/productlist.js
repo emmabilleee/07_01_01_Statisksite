5cd980e93977ccf2e53f3e6f5cffc2165ce64549
Emma <emmabille16@gmail.com>
2023-08-28 15:11:47 +0200

    sale

--- js/productlist.js
+++ js/productlist.js
@@ -18,14 +18,20 @@ function showProduct(product) {
   copy.querySelector("h3").textContent = product.productdisplayname;
   if (product.soldout) {
     copy.querySelector("article").classList.add("soldout");
+    copy.querySelector(".brand").textContent = product.soldout;
   }
   copy.querySelector("img").src = `https://kea-alt-del.dk/t7/images/webp/640/${product.id}.webp`;
   copy.querySelector(".brand").textContent = product.brandname;
   copy.querySelector(".subcat").textContent = product.articletype;
   copy.querySelector(".price").textContent = product.price;
   if (product.discount) {
-    copy.querySelector("article").classList.add("sale");
+    copy.querySelector("article").classList.add("sales");
+    copy.querySelector("article").classList.add("pricediscounted");
+    copy.querySelector(".brand").textContent = product.discount;
   }
+
+  copy.querySelector(".discount").textContent = product.discount;
+
   //Append
   document.querySelector("main").appendChild(copy);
 }
